9cab18ab83b2d2b25f655dc1dcb5818b7f4ca5a4
suleman81 <sunnybajwa904@gmail.com>
2023-05-25 17:45:38 +0500

    commit

--- app/src/main/java/com/moutamid/onlinestore/activities/buyer_side/AllProductActivity.java
+++ app/src/main/java/com/moutamid/onlinestore/activities/buyer_side/AllProductActivity.java
@@ -2,15 +2,110 @@ package com.moutamid.onlinestore.activities.buyer_side;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.fxn.stash.Stash;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.moutamid.onlinestore.MainActivity;
 import com.moutamid.onlinestore.R;
+import com.moutamid.onlinestore.adapter.ProductAdapter;
+import com.moutamid.onlinestore.adapter.SearchProductAdapter;
+import com.moutamid.onlinestore.constants.Constants;
+import com.moutamid.onlinestore.databinding.ActivityAllProductBinding;
+import com.moutamid.onlinestore.models.ProductModel;
+
+import java.util.ArrayList;
 
 public class AllProductActivity extends AppCompatActivity {
-
+    ActivityAllProductBinding binding;
+    ArrayList<ProductModel> productList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_product);
+        binding = ActivityAllProductBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        productList = new ArrayList<>();
+
+        binding.header.title.setText("Products");
+
+        binding.header.back.setOnClickListener(v -> {
+            startActivity(new Intent(this, BuyerMainActivity.class));
+            finish();
+        });
+
+        if (Stash.getBoolean(Constants.isSEARCH, false)){
+            showCat();
+        } else {
+                showAll();
+        }
+    }
+
+    private void showCat() {
+        String cat = Stash.getString(Constants.SEARCH);
+        binding.header.title.setText("Search for \"" + cat + "\"");
+        Constants.databaseReference().child(Constants.Product)
+                .get().addOnSuccessListener(dataSnapshot -> {
+                    if (dataSnapshot.exists()){
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            for (DataSnapshot ss : snapshot.getChildren()) {
+                                ProductModel productModel = ss.getValue(ProductModel.class);
+                                if (cat.equals(productModel.getCategory())) {
+                                    productList.add(productModel);
+                                }
+                            }
+                        }
+
+                        if (productList.size() == 0) {
+                            Snackbar.make(AllProductActivity.this, binding.root, "No Product Found", Snackbar.LENGTH_INDEFINITE).show();
+                        } else {
+                            binding.recyler.setVisibility(View.VISIBLE);
+                            binding.loading.setVisibility(View.GONE);
+                        }
+
+                    } else {
+                        Snackbar.make(AllProductActivity.this, binding.root, "No Product Found", Snackbar.LENGTH_INDEFINITE).show();
+                    }
+
+                    Constants.dismissDialog();
+                    SearchProductAdapter adapter = new SearchProductAdapter(AllProductActivity.this, productList);
+                    binding.recyler.setAdapter(adapter);
+                }).addOnFailureListener(e -> {
+                    Constants.dismissDialog();
+                    Snackbar.make(AllProductActivity.this, binding.root, "Something went wrong", Snackbar.LENGTH_SHORT).show();
+                });
+    }
+
+    private void showAll() {
+        Constants.databaseReference().child(Constants.Product)
+                .get().addOnSuccessListener(dataSnapshot -> {
+                    if (dataSnapshot.exists()){
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            for (DataSnapshot ss : snapshot.getChildren()) {
+                                ProductModel productModel = ss.getValue(ProductModel.class);
+                                productList.add(productModel);
+                            }
+                        }
+                        binding.recyler.setVisibility(View.VISIBLE);
+                        binding.loading.setVisibility(View.GONE);
+                    } else {
+                        Snackbar.make(AllProductActivity.this, binding.root, "No Product Found", Snackbar.LENGTH_INDEFINITE).show();
+                    }
+                    Constants.dismissDialog();
+                    SearchProductAdapter adapter = new SearchProductAdapter(AllProductActivity.this, productList);
+                    binding.recyler.setAdapter(adapter);
+                }).addOnFailureListener(e -> {
+                    Constants.dismissDialog();
+                    Snackbar.make(AllProductActivity.this, binding.root, "Something went wrong", Snackbar.LENGTH_SHORT).show();
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, BuyerMainActivity.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/moutamid/onlinestore/adapter/SearchProductAdapter.java
@@ -0,0 +1,138 @@
+package com.moutamid.onlinestore.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.fxn.stash.Stash;
+import com.moutamid.onlinestore.R;
+import com.moutamid.onlinestore.activities.buyer_side.ProductDetailActivity;
+import com.moutamid.onlinestore.constants.Constants;
+import com.moutamid.onlinestore.models.CartModel;
+import com.moutamid.onlinestore.models.ProductModel;
+
+import java.util.ArrayList;
+
+public class SearchProductAdapter extends RecyclerView.Adapter<SearchProductAdapter.CategoryVH> {
+
+    Context context;
+    ArrayList<ProductModel> list;
+
+    public SearchProductAdapter(Context context, ArrayList<ProductModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public CategoryVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CategoryVH(LayoutInflater.from(context).inflate(R.layout.search_product_layout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryVH holder, int position) {
+        ProductModel model = list.get(holder.getAbsoluteAdapterPosition());
+        holder.name.setText(model.getName());
+        Glide.with(context).load(model.getThumbnail()).into(holder.imageView);
+        holder.price.setText("$" + model.getPrice());
+
+        ArrayList<ProductModel> bookmarkModels = Stash.getArrayList(Constants.favrt, ProductModel.class);
+        ArrayList<CartModel> carttt = Stash.getArrayList(Constants.CART, CartModel.class);
+
+
+        for (ProductModel fvrtModel : bookmarkModels){
+            if (fvrtModel.getID().equals(model.getID())){
+                holder.fav.setImageResource(R.drawable.favorite);
+                holder.isFavrt = true;
+            }
+        }
+
+        for (CartModel cartModel : carttt){
+            if (cartModel.getID().equals(model.getID())){
+                holder.isAdded = true;
+            }
+        }
+
+        holder.fav.setOnClickListener(v -> {
+            ArrayList<ProductModel> favrtList = Stash.getArrayList(Constants.favrt, ProductModel.class);
+            if (holder.isFavrt){
+                for (int i = 0; i < favrtList.size(); i++) {
+                    if (favrtList.get(i).getID().equals(model.getID())) {
+                        Toast.makeText(context, "removed", Toast.LENGTH_SHORT).show();
+                        favrtList.remove(i);
+                    }
+                }
+                holder.fav.setImageResource(R.drawable.round_favorite_border_24);
+                holder.isFavrt = false;
+                Stash.put(Constants.favrt, favrtList);
+            } else {
+                Toast.makeText(context, "added", Toast.LENGTH_SHORT).show();
+                favrtList.add(model);
+                Stash.put(Constants.favrt, favrtList);
+                holder.fav.setImageResource(R.drawable.favorite);
+                holder.isFavrt = true;
+            }
+        });
+
+        holder.itemView.setOnClickListener(v -> {
+            Stash.put(Constants.MODEL, model);
+            context.startActivity(new Intent(context, ProductDetailActivity.class));
+        });
+
+        holder.add.setOnClickListener(v -> {
+            Constants.initDialog(context);
+            Constants.showDialog();
+            new Handler().postDelayed(() -> {
+                ArrayList<CartModel> cart = Stash.getArrayList(Constants.CART, CartModel.class);
+                CartModel cartModel = new CartModel(model.getID(), model, 1);
+                if (!holder.isAdded){
+                    cart.add(cartModel);
+                    Stash.put(Constants.CART, cart);
+                    holder.isAdded = true;
+                } else {
+                    Toast.makeText(context, "Already Added into your cart", Toast.LENGTH_SHORT).show();
+                }
+                Constants.dismissDialog();
+            }, 1500);
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class CategoryVH extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView name, price;
+        Button add;
+        ImageButton fav;
+        boolean isFavrt, isAdded;
+        public CategoryVH(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            imageView = itemView.findViewById(R.id.image);
+            price = itemView.findViewById(R.id.price);
+            fav = itemView.findViewById(R.id.fav);
+            add = itemView.findViewById(R.id.add);
+
+            isFavrt = false;
+            isAdded = false;
+
+        }
+    }
+
+}
